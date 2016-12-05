d9aaf4b7dfa1c31b29151a7c72c0af761ff1f9ab
Fondify Marketing <Marketing@MBP-de-Fondify.cybercable.net.mx>
2016-12-05 13:18:44 -0600

    configurating the Project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205185950) do
+ActiveRecord::Schema.define(version: 20161205191154) do
+
+  create_table "projects", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.text     "short_description"
+    t.text     "description"
+    t.string   "image_url"
+    t.string   "status",                                    default: "pending"
+    t.decimal  "goal",              precision: 8, scale: 2
+    t.datetime "expiration_date"
+    t.datetime "created_at",                                                    null: false
+    t.datetime "updated_at",                                                    null: false
+  end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
