b84ad6b77eb3e1089a0d8ced286f4ef765e564f8
Fondify Marketing <Marketing@MBP-de-Fondify.cybercable.net.mx>
2016-12-06 14:27:51 -0600

    New reward form added

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -10,10 +10,25 @@ class RewardsController < ApplicationController
     end
   end
 
+  def create
+    @reward = @project.rewards.build(reward_params)
+    respond_to do |format|
+      if @reward.save
+        format.html { redirect_to @project, notice: "Reward was successfully created" }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
   private
 
   def set_project
     @project = Project.find(params[:project_id])
+  end
+
+  def reward_params
+    params.require(:reward).permit(:name, :description, :value, :shipping, :number_available, :estimated_delivery)
   end
 
 end
