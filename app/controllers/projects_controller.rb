b1bf8d3b94d223c23fdd084f7c09fb173e0c6777
Fondify Marketing <Marketing@MBP-de-Fondify.cybercable.net.mx>
2016-12-06 11:38:00 -0600

    create new projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,6 +19,7 @@ class ProjectsController < ApplicationController
 
   #prevent user from accesing projects when they're noy logged in
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
@@ -33,6 +34,31 @@ class ProjectsController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    @project = current_user.projects.build(projects_params)
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to @project, notice: "Project was successfully created" }
+        format.json { render :show, status: :ok, location: @project}
+      else
+        format.html { redirect_to :edit}
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def projects_params
+    params.require(:project).permit(:name, :short_description, :description, :goal, :image_url, :expiration_date)
   end
 
 end
